499f25750b4ead83bbbc8ae5c7537de46af8c892
KnightOfBlackLily <amadeys150993@yandex.ru>
2020-02-18 22:08:01 +0300

    Update main.dart

--- flexible/main.dart
+++ flexible/main.dart
@@ -104,3 +104,21 @@ class MyWidget extends StatelessWidget {
     );
   }
 }
+
+class TestWidget extends StatelessWidget {
+  final String text;
+  final Color color;
+  TestWidget(this.text, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      padding: EdgeInsets.all(16),
+      child: Text(
+        text ?? "",
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
