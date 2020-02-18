c5ab6938d9120ac4fd5abc83894b976f57470244
KnightOfBlackLily <amadeys150993@yandex.ru>
2020-02-18 21:59:14 +0300

    Add Flexible example

---
+++ flexible/test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TestWidget extends StatelessWidget {
+  final String text;
+  final Color color;
+  TestWidget(this.text, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      padding: EdgeInsets.all(16),
+      child: Text(
+        text ?? "",
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
